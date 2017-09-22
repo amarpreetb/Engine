567fcb083e9ceb78c236d484e4a826907cef0d20
Amarpreet B <amarpreet.bhachu@gmail.com>
2017-09-22 19:45:02 -0400

    Day 2

--- GameContainer.java
+++ GameContainer.java
@@ -3,21 +3,66 @@ package com.AMax.Engine;
 public class GameContainer implements Runnable{
 
     private Thread thread;
+    private Window window;
+    private Renderer renderer;
 
     private boolean running = false;
     private final double Update_Cap = 1.0/60.0;
+    private int width = 1200, height = 800;
+    private float scale = 1f;
+    private String title = "Amax Engine";
 
     public  GameContainer(){
 
     }
 
     public void start(){
+
+        window = new Window(this);
         thread = new Thread(this);
+        renderer = new Renderer(this);
         thread.run();
 
     }
 
+    public Window getWindow() {
+        return window;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public void stop(){
+
 
     }
 
@@ -53,7 +98,7 @@ public class GameContainer implements Runnable{
 
                 if (frameTime >= 1.0){
                     frameTime = 0;
-                    frames = 0;
+                    //frames = 0;
                     fps = frames;
                     frames = 0;
 
@@ -62,6 +107,9 @@ public class GameContainer implements Runnable{
             }
 
             if(render){
+                renderer.clear();
+                window.update();
+                frames++;
 
             }
             else {
---
+++ Renderer.java
@@ -0,0 +1,29 @@
+package com.AMax.Engine;
+
+import java.awt.image.DataBufferInt;
+
+public class Renderer {
+
+    private  int pixelWidth, pixelHeight;
+    private  int[] pixel;
+
+    public Renderer(GameContainer gc){
+
+        pixelWidth = gc.getWidth();
+        pixelHeight = gc.getHeight();
+
+        pixel = ((DataBufferInt)gc.getWindow().getImage().getRaster().getDataBuffer()).getData();
+
+
+    }
+
+    public void clear(){
+
+        for (int i = 0;i < pixel.length; i++){
+            pixel[i] += i;
+
+        }
+
+
+    }
+}
