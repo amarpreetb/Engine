0bcf5a2fb6b216b1eb302d9cfa6c332b91d009a1
Amarpreet B <amarpreet.bhachu@gmail.com>
2017-09-24 17:32:42 -0400

    Image on screen

--- Game/GameManager.java
+++ Game/GameManager.java
@@ -4,23 +4,31 @@ import com.AMax.Engine.AbstractGame;
 import com.AMax.Engine.GameContainer;
 import com.AMax.Engine.Renderer;
 
+import java.awt.*;
 import java.awt.event.KeyEvent;
+import com.AMax.Engine.gfx.Image;
+
 
 public class GameManager extends AbstractGame{
 
-    public GameManager(){
+    private Image image;
 
+
+    public GameManager(){
+        image = new Image("/Untitled.png");
     }
 
     @Override
     public void update(GameContainer gc, float dt) {
-        if (gc.getInput().isKeyDown(KeyEvent.VK_A)){
+        if (gc.getInput().isKey(KeyEvent.VK_A)){
             System.out.println("A");
         }
     }
 
     @Override
     public void render(GameContainer gc, Renderer r) {
+    	
+    	r.drawImage(image, gc.getInput().getMouseX() - 32, gc.getInput().getMouseY() - 32);
 
     }
 
