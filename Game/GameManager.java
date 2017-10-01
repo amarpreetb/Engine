830523af66706545b313a78f5e6dbd928c9ebc19
Amarpreet B <amarpreet.bhachu@gmail.com>
2017-10-01 17:30:24 -0400

    Added Sound and Alha rendering

--- Game/GameManager.java
+++ Game/GameManager.java
@@ -6,27 +6,37 @@ import com.AMax.Engine.Renderer;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+
+import com.AMax.Engine.audio.SoundClip;
 import com.AMax.Engine.gfx.Image;
 import com.AMax.Engine.gfx.ImageTile;
+import com.AMax.game.GameManager;
 
 
 public class GameManager extends AbstractGame{
 
-    private ImageTile image;
+    private Image image;
+    private Image image2;
+    private SoundClip clip;
 
 
     public GameManager(){
 
-        image = new ImageTile("/Untitled.png", 16, 16);
+        image = new ImageTile("/index.png", 16, 16);
+        image2 = new Image("/Untitled.png");
+        clip = new SoundClip("/cena.wav");
+        image2.setAlpha(true);
+        //clip.setVolume(-20); //Volume Adjuster
     }
 
     @Override
     public void update(GameContainer gc, float dt) {
-        if (gc.getInput().isKey(KeyEvent.VK_A)){
-            System.out.println("A");
+    	if (gc.getInput().isKey(KeyEvent.VK_A)){
+            //System.out.println("A");
+            clip.play();
         }
 
-        temp += dt * 20;
+        temp += dt;
         if(temp > 3){
             temp = 0;
         }
@@ -36,12 +46,17 @@ public class GameManager extends AbstractGame{
     @Override
     public void render(GameContainer gc, Renderer r) {
     	
-    	r.drawImageTile(image, gc.getInput().getMouseX() - 16, gc.getInput().getMouseY() - 16, (int)temp, 0);
-
+    	r.drawImage(image2, 10, 10);
+    	r.drawImage(image, gc.getInput().getMouseX(), gc.getInput().getMouseY());
+        //r.drawFillRect(10, 10, 32, 32, 0xffffccff);
+        //r.drawFillRect(gc.getInput().getMouseX() - 16, gc.getInput().getMouseY() - 16, 32, 32, 0xffffccff);
     }
 
     public static void main(String[] args){
         GameContainer gc = new GameContainer(new GameManager());
+        gc.setWidth(320);
+        gc.setWidth(240);
+        gc.setScale(3f);
         gc.start();
     }
 }
